235147233c6f9afc5066abd20cca922768486512
towfd <40749931+towfd@users.noreply.github.com>
2026-06-24 11:18:16 +0800

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # Sliding window
+        # 建立兩個指針，一個指針先往前跑，邊跑邊將字母輸入進set內，並計數
+        # 若右指針遇到重複的字母，左指針開始跑，跑到吐掉重複的字母為止，持續到全部的字母跑完
+        # 回傳最大計數
+        # 時間複雜度 O(n)
+        # 空間複雜度 O(n)
+
+        lindex, rindex = 0, 0
+        sset = set()
+        max_len = 0
+        while rindex != (len(s)):
+            if s[rindex] in sset:
+                while True:
+                    sset.remove(s[lindex])
+                    if s[lindex] == s[rindex]:
+                        lindex += 1
+                        break
+                    lindex += 1
+            sset.add(s[rindex])
+            if len(sset) > max_len:
+                max_len = len(sset)
+            rindex += 1
+        return max_len
