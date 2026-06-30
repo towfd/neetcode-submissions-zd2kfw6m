9899c4a0af92f32dae7b9914a279b64cc303bf6c
towfd <40749931+towfd@users.noreply.github.com>
2026-06-30 10:58:37 +0800

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # 暴力破解
+        # 針對每個遞迴到的符號，去尋找是否有相對應的閉環，找到後將其移除
+        # 若最後字串不為空 回傳False
+        # 時間複雜度 O(N^2)
+        # 空間複雜度 O(1)
+        while '()' in s or '[]' in s or "{}" in s:
+            s = s.replace('()', '')
+            s = s.replace("{}",'')
+            s = s.replace('[]','')
+        if s:
+            return False
+        else:
+            return True
