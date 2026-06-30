e5fe7e6a0ea6115e147e48028b1cd80df989f47a
towfd <40749931+towfd@users.noreply.github.com>
2026-06-30 11:34:46 +0800

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,18 @@
+class MinStack:
+    # 暴力破解，getMin使用min函數直接回傳，花費O(n)時間
+    
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+        
