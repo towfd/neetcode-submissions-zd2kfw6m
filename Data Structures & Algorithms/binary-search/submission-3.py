1ee811162a2229ca80931fb3dacec6d016f58f99
towfd <40749931+towfd@users.noreply.github.com>
2026-07-03 09:39:55 +0800

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # 建立兩個指標，一左一右，直到left > right
+        # 找到就return index 沒找到就return -1
+        # 時間複雜度O(logn)
+        # 空間複雜度O(1)
+
+        l_idx, r_idx = 0, len(nums) - 1
+
+        while l_idx <= r_idx:
+            if nums[l_idx] == target:
+                return l_idx
+            elif nums[r_idx] == target:
+                return r_idx
+            
+            l_idx += 1
+            r_idx -= 1
+        return -1
